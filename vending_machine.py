c915869234e5338cbd3dfb2d0eada8c9b6d2c2e8
keisuke <keisukeseto.89103@gmail.com>
2024-11-14 23:12:37 +0900

    refactor:クラス毎にファイルを作成

---
+++ vending_machine.py
@@ -0,0 +1,60 @@
+from juice import Juice
+from suica import Suica
+
+class VendingMachine:
+    def __init__(self):
+        pepsi = Juice("ペプシ", 150)
+        self.product_list = [{"juice": pepsi, "stock": 5}]
+        self._total_sales = 0
+
+    @property
+    def total_sales(self):
+        return self._total_sales
+
+    @total_sales.setter
+    def total_sales(self, total_sales):
+        self._total_sales = total_sales
+
+    def sales_judge(self, suica):
+        available_list = [i["juice"].name for i in self.product_list if suica.balance >= i["juice"].price] # check because i fixed.
+        return f"現在買える商品は{available_list}です。"
+
+    def show_stock(self):
+        stock_list = [f"{i["juice"].name}:{i["stock"]}本" for i in self.product_list]
+        return f"現在の在庫状況は{stock_list}です。"
+
+    def lineup_expand(self):
+        existing_names = {i["juice"].name for i in self.product_list}
+        new_products = [("いろはす", 120), ("モンスター", 230)]
+        for name, price in new_products:
+            if name not in existing_names:
+                self.product_list.append({"juice": Juice(name, price), "stock": 5})
+        return f"商品を拡充して、現在{[i["juice"].name for i in self.product_list]}を販売中。"
+        
+    def add_stock(self, product, stock):
+        for i in self.product_list:
+            if i["juice"].name == product:
+                i["stock"] += stock
+                return f"{i["juice"].name}の在庫に{stock}本追加して、現在の在庫は{i["stock"]}本。"
+
+    def purchase(self, suica, product):
+        self.suica = suica
+        self.product = [i for i in self.product_list if i["juice"].name == product][0]
+        try:
+            if self.suica.balance < self.product["juice"].price:
+                raise Exception(f"Suicaのチャージ残高が不足しております。")
+            if self.product["stock"] == 0:
+                raise Exception(f"{product}の在庫がありません。")
+            balance = self.suica.balance
+            balance -= self.product["juice"].price
+            self.suica.balance = balance
+            total_sales = self.total_sales
+            total_sales += self.product["juice"].price
+            self.total_sales = total_sales
+            self.product["stock"] -= 1
+            for i in self.product_list:
+                if i["juice"].name == product:
+                    i["stock"] = self.product["stock"]
+                    return f"{i["juice"].name}の購入が完了しました。"
+        except Exception as e:
+            return e
